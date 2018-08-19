af0f07aba7b51775c0230290816d987e9138d656
Worameth Semapat <31945018+chen2584@users.noreply.github.com>
2018-08-19 15:13:31 +0700

    first init

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using McMaster.Extensions.CommandLineUtils;
+
+namespace ConsoleTips
+{
+    [Command(Name = "di", Description = "Sample project")]
+    //[HelpOption("--my-custom-help-option")] // Change from default --help to --my-custom-help-option
+    class Program
+    {
+        [Argument(0, Description="Your FirstName")]
+        [Required]
+        public string FirstName { get; }
+
+        [Argument(1)]
+        public string LastName { get; }  // this one is optional because it doesn't have `[Required]`
+
+        [Option(Description="Middle Name")]
+        [Required]
+        public string MiddleName { get; }
+        private void OnExecute()
+        {
+            Console.WriteLine($"Hello {FirstName} {MiddleName} {LastName}");
+        }
+
+        static void Main(string[] args)
+        {
+            CommandLineApplication.Execute<Program>(args);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+### dotnet-console-tips
+https://odetocode.com/blogs/scott/archive/2018/08/16/three-tips-for-console-applications-in-net-core.aspx
+
+# Package
+```
+McMaster.Extensions.CommandLineUtils
+```
+https://natemcmaster.github.io/CommandLineUtils/docs/intro.html
